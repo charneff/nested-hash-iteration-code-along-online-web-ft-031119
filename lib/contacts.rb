680a603f765dc6c589ba80d714cc3f9d48b8ab20
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-12 00:40:31 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,7 @@ contacts = {
 contacts.each do |person, contact_details_hash|
   contact_details_hash.each do |attribute, data|
     binding.pry
+  end
 end
 
   # This is the array we will be passing into the remove_strawberry method
