3294fa4831b29d15e3841d8e82e4c0fd1468b9ea
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-12 00:46:24 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,13 +13,6 @@ contacts = {
   }
 }
 
-contacts.each do |person, contact_details_hash|
-  contact_details_hash.each do |attribute, data|
-    if attribute == :favorite_ice_cream_flavors
-      binding.pry
-    end
-  end
-end
 
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
@@ -36,5 +29,11 @@ end
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
